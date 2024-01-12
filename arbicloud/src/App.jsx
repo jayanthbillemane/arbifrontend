93cbd0a8602bb28692485bce7be4ea1b6189675c
jayanthbillemane <30498535+jayanthbillemane@users.noreply.github.com>
2024-01-12 17:59:57 +0530

    model frontend update

--- arbicloud/src/App.jsx
+++ arbicloud/src/App.jsx
@@ -1,25 +1,50 @@
-import { useState, useEffect } from 'react';
-import Details from './components/Details'
+import React from 'react';
+import CameraApp from './components/CameraApp';
+import VideoStream from './components/VideoStream'
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    // Replace 'YOUR_BACKEND_API_URL' with the actual URL of your backend API
-    const backendApiUrl = 'http://localhost:8000/';
-
-    fetch(`${backendApiUrl}`)
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
   return (
-    <div>
-      <h1>Hello from React Vite App</h1>
-      {data && <p>Data from Backend: {data.message}</p>}
-      <Details/>
+    <div className="App">
+      <CameraApp />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState, useEffect } from 'react';
+// import Details from './components/Details'
+// function App() {
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     // Replace 'YOUR_BACKEND_API_URL' with the actual URL of your backend API
+//     const backendApiUrl = 'http://localhost:8000/';
+
+//     fetch(`${backendApiUrl}`)
+//       .then((response) => response.json())
+//       .then((data) => setData(data))
+//       .catch((error) => console.error('Error fetching data:', error));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Hello from React Vite App</h1>
+//       {data && <p>Data from Backend: {data.message}</p>}
+//       <Details/>
+//     </div>
+//   );
+// }
+
+// export default App;
